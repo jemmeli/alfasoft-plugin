3473549090b8005c127ac435390ad2fa75d443f5
jemmeli <jemmeli84@gmail.com>
2024-03-01 12:40:17 +0000

    add activate and deactivated hooks for the plugin

--- contacts.php
+++ contacts.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * Plugin Name: Contacts Plugin
-* Plugin URI: https://wordpress.org/plugins/Viager/
+* Plugin URI: https://wordpress.org/plugins/Alfasoft/
 * Description: Contact Management Plugin.
 * Version: 0.0.1
 * Author: Made with Jemmeli Nejmeddine
@@ -9,6 +9,9 @@
 * Author Email: jemmeli84@gmail.com
 */
 
+/*======================
+===Styles & Scripts=====
+=======================*/
 //styles
 function contacts_plugin_enqueue_styles() {
     //bootstrap
@@ -37,4 +40,73 @@ function contacts_script_footer(){
 	wp_localize_script( 'ajaxHandle', 'ajax_object', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 } 
 
-add_action('wp_footer', 'add_this_script_footer');
+add_action('wp_footer', 'contacts_script_footer');
+
+
+/*======================
+===General Func=========
+=======================*/
+function cc($var){
+	echo "<pre>";
+	print_r($var); 
+	echo "</pre>";
+	die();
+}
+
+/*======================
+===plugin activated=====
+=======================*/
+function contacts_plugin_activate(){
+
+    global $wpdb;
+    
+    $person = $wpdb->prefix . 'person';
+	$charset_collate = $wpdb->get_charset_collate();
+	$sql = "CREATE TABLE $person (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		name VARCHAR(255) NOT NULL,
+		email VARCHAR(255) NOT NULL,
+        avatar_url VARCHAR(255),
+        UNIQUE KEY email_unique (email),
+		PRIMARY KEY (id)
+	) $charset_collate;";
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+
+    $contact = $wpdb->prefix . 'contact';
+	$charset_collate = $wpdb->get_charset_collate();
+	$sql = "CREATE TABLE $contact (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		person_id INT NOT NULL,
+        country_code VARCHAR(10) NOT NULL,
+        number VARCHAR(9) NOT NULL,
+		PRIMARY KEY (id),
+        UNIQUE KEY contact_unique (country_code, number),
+        FOREIGN KEY (person_id) REFERENCES " . $wpdb->prefix . "person(id) ON DELETE CASCADE
+	) $charset_collate;";
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+}
+register_activation_hook(__FILE__ , "contacts_plugin_activate");
+
+/*======================
+===plugin Deactivated===
+=======================*/
+function contacts_plugin_deactivate(){
+
+    global $wpdb;
+
+    //delete person table
+    $person = $wpdb->prefix . "person";
+    $sql = "DROP TABLE IF EXISTS $person";
+    $wpdb->query($sql);
+    delete_option("my_plugin_db_version");
+
+
+    //delete contact table
+    $contact = $wpdb->prefix . "contact";
+    $sql = "DROP TABLE IF EXISTS $contact";
+    $wpdb->query($sql);
+    delete_option("my_plugin_db_version");
+}
+register_deactivation_hook(__FILE__ , "contacts_plugin_deactivate");
