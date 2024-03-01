c4ba9f6d5597e467c9e1d0353d081ea885bb3883
jemmeli <jemmeli84@gmail.com>
2024-03-01 11:52:57 +0000

    first commit

---
+++ contacts.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Plugin Name: Contacts Plugin
+* Plugin URI: https://wordpress.org/plugins/Viager/
+* Description: Contact Management Plugin.
+* Version: 0.0.1
+* Author: Made with Jemmeli Nejmeddine
+* Author URI: https://github.com/jemmeli
+* Author Email: jemmeli84@gmail.com
+*/
